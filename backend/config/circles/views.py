3bd898a1e0500456ad5522c2b7a2c57f3b3cf870
Danil Melinkov <danil369.ru@mail.ru>
2021-11-11 07:11:47 +0600

    Circle View

--- backend/config/circles/views.py
+++ backend/config/circles/views.py
@@ -1,3 +1,42 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
 from django.shortcuts import render
 
-# Create your views here.
+from . import forms
+from . import models
+
+
+def create_update_circles(request, context):
+    form = forms.CircleForm(request.POST or None)
+
+    if form.is_valid():
+        circle = form.save(commit=False)
+        circle.user = request.user
+        circle.save()
+
+    context["create_circle"] = form
+
+
+def list_objects(request, context):
+    context["circles"] = models.Circle.objects.filter(user=request.user)
+
+
+def delete_objects(request, context):
+    id_to_delete = request.POST.get("id")
+
+    if id_to_delete and id_to_delete.isdigit():
+        models.Circle.objects.filter(id=id_to_delete).delete()
+    elif id_to_delete == "all":
+        models.Circle.objects.all().delete()
+
+
+@login_required
+def main(request):
+    context = {}
+    list_objects(request, context)
+    create_update_circles(request, context)
+    delete_objects(request, context)
+
+    if request.method == "POST":
+        return redirect(request.META.get('HTTP_REFERER'))
+    return render(request, "circles/main.html", context)
